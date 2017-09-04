c375430c734ee9e555da93bd132bef54869a1a37
acorn <acorn@acorn-PC>
2017-09-04 16:15:05 +0900

    membeservlet 추가

---
+++ RandomBox/src/com/controller/member/MemberAddServlet.java
@@ -0,0 +1,60 @@
+package com.controller.member;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/MemberAddServlet")
+public class MemberAddServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		String ox = request.getParameter("ox");
+		String userid = request.getParameter("userid");
+		String username = request.getParameter("username");
+		String passwd = request.getParameter("passwd");
+		String email1 = request.getParameter("email1");
+		String email2 = request.getParameter("email2");
+		String phone1 = request.getParameter("phone1");
+		String phone2 = request.getParameter("phone2");
+		String phone3 = request.getParameter("phone3");
+		String gender = request.getParameter("gender");
+		String birthday = request.getParameter("birthday");
+		String post1 = request.getParameter("post1");
+		String post2 = request.getParameter("post2");
+		String addr1 = request.getParameter("addr1");
+		String addr2 = request.getParameter("addr2");
+		String email = email1 + email2;
+		String phone = phone1 + phone2 + phone3;
+		
+		/*MemberDTO dto = new MemberDTO;
+		dto.setOx(ox);
+		dto.setUserid(userid);
+		dto.setUsername(username);
+		dto.setPasswd(passwd);
+		dto.setEmail(email);
+		dto.setGender(gender);
+		dto.setBirthday(birthday);
+		dto.setPost1(post1);
+		dto.setPost2(post2);
+		dto.setPhone(phone);
+		dto.setAddr1(addr1);
+		dto.setAddr2(addr2);
+
+		MemberService service = new MemberService
+		*/
+		
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
