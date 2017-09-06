cc4eeaa50f076dd3b74234c2f25042a842660a3d
user <user@user-PC>
2017-09-06 18:20:13 +0900

    pull받기

--- RandomBox/src/com/controller/member/mypage/MyPageFrontServlet.java
+++ RandomBox/src/com/controller/member/mypage/MyPageFrontServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.dto.MemberDTO;
 import com.exception.MyException;
@@ -18,63 +19,91 @@ public class MyPageFrontServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		String userid = request.getParameter("userid");
-		System.out.println(userid);
-		
-		String requestURI =request.getRequestURI();
-		String contextPath = request.getContextPath();
-		String command = requestURI.substring(contextPath.length());
-		
-		String target = "";
-		MyPageService mservice = new MyPageService();
-		
-		if(command.equals("/userinfo.do")) {
 
+		HttpSession session = request.getSession();
+		//session받자
+		MemberDTO logindto = (MemberDTO)session.getAttribute("login");
+		//session에 저장된 login하고 mypage를 눌렀을때 넘어오는 login
+		String target = null;
+		MyPageService service = new MyPageService();
+		String user ;
+		
+		if (logindto != null) {
+			System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>");
+			target = "myPage1.jsp";
+			System.out.println("front servlet"+logindto);
 			try {
-				MemberDTO dto = mservice.myPageUserInfo(userid);
-				request.setAttribute("dto", dto);
-			} catch (MyException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				
-			}
+				user = (service.
+				//userid로주고 userdto로받음
 			
-			
-			target="myPage/myPageUserInfo.jsp";
-		}
-		if(command.equals("/orderinfo.do")) {
-			target="myPage/myPageOrderInfo.jsp";
-		}
-		if(command.equals("/writelist.do")) {
-			target = "myPage/myPageWriteList.jsp";
-		}
-		if(command.equals("/sellinfo.do")) {
-			target = "myPage/myPageSellInfo.jsp";
-			
-		}
-		if(command.equals("/goodsinfo.do")) {
-			target = "myPage/myPageGoodsInfo.jsp";
-			
-		}
-		if(command.equals("/userinfoupdate.do")) {
-			System.out.println("userinfoupdate");
-			String passwd = request.getParameter("passwd");
-			MemberDTO dto = new MemberDTO();
-			dto.setPasswd(passwd);
-			
-			try {
-				mservice.updateuserinfo(dto);
+				session.setAttribute("login", user);
 			} catch (MyException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+		}else {
+			target="LoginFormServlet";
 		}
 
-		
 
+		MemberDTO loginuser = (MemberDTO) session.getAttribute("login");
+
+
+		if (loginuser != null) {
+
+			String requestURI = request.getRequestURI();
+			String contextPath = request.getContextPath();
+			String command = requestURI.substring(contextPath.length());
+
+			MyPageService mservice = new MyPageService();
+
+			if (command.equals("/userinfo.do")) {
+				try {
+					MemberDTO userdto = mservice.myPageUserInfo(userid);
+					request.setAttribute("dto", dto);
+				} catch (MyException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				target = "myPage/myPageUserInfo.jsp";
+			}
+
+			if (command.equals("/orderinfo.do")) {
+				target = "myPage/myPageOrderInfo.jsp";
+			}
+
+			if (command.equals("/writelist.do")) {
+				target = "myPage/myPageWriteList.jsp";
+			}
+
+			if (command.equals("/sellinfo.do")) {
+				target = "myPage/myPageSellInfo.jsp";
+			}
+
+			if (command.equals("/goodsinfo.do")) {
+				target = "myPage/myPageGoodsInfo.jsp";
+			}
+
+			if (command.equals("/userinfoupdate.do")) {
+				System.out.println("userinfoupdate");
+				String passwd = request.getParameter("passwd");
+				MemberDTO dto = new MemberDTO();
+				dto.setPasswd(passwd);
+
+				try {
+					mservice.updateuserinfo(dto);
+				} catch (MyException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+		} else {
+			target = "MyPageServlet";
+		}
 
 		RequestDispatcher dis = request.getRequestDispatcher(target);
-		dis.forward(request,response);
+		dis.forward(request, response);
 
 	}
 
